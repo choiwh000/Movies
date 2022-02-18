f69e6273c16a614745869675b2c9b153c454f533
choiwh000 <cwh915@naver.com>
2022-02-18 19:43:56 +0900

    Update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,13 @@
 import Movie from "../components/Movie";
 import { useState, useEffect } from "react";
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
+import useInterval from "use-interval";
 
 function Home() {
   const [loading, setLoading] = useState(true);
   const [movies, setMovies] = useState([]);
+  const [num, setNum] = useState(0);
   const getMovies = async () => {
     const json = await (
       await fetch(
@@ -16,12 +20,47 @@ function Home() {
   useEffect(() => {
     getMovies();
   }, []);
+
+  useInterval(() => {
+    if (num === movies.length - 1) {
+      setNum(0);
+    } else {
+      setNum(num + 1);
+    }
+  }, 10000);
+
   return (
     <div>
+      <header>
+        <Link to={`/Movies`} id={styles.logo}>
+          Logo
+        </Link>
+        <ul id={styles.home_ul}>
+          <li>
+            <Link to={`/Movies`}>Main</Link>
+          </li>
+          <li>
+            <Link to={`/movie/category`}>Category</Link>
+          </li>
+          <li>
+            <Link to={`/movie/My`}>My</Link>
+          </li>
+        </ul>
+      </header>
+      <div>{num}</div>
+      <div>
+        {loading ? (
+          <h1>loading...</h1>
+        ) : (
+          <div>
+            <img src={movies[num].large_cover_image} />
+          </div>
+        )}
+      </div>
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div>
+        <div className={styles.movies}>
           {movies.map((movie) => (
             <Movie
               key={movie.id}
